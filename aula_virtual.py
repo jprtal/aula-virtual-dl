65df3e7fb5e168ec1d7049b80599b7fbae93bd0c
J Portal <j.portal@outlook.com>
2020-04-17 18:12:44 +0200

    Minimal improvements

--- aula_virtual.py
+++ aula_virtual.py
@@ -4,22 +4,23 @@
 # Copyright (C) 2020 Jorge Portal
 # This script is under MIT license
 #
-# Version: 2020.03.27
+# Version: 2020.04.17
 # You can find new versions and fixes of this script over the time at
 # https://github.com/jprtal/aula-virtual-dl
 
-import http.cookiejar as cookielib
-from bs4 import BeautifulSoup as bs
-import cgi
-import mechanize
-import os
-import getpass
 import argparse
-from urllib.parse import unquote
-from pathvalidate import sanitize_filename
-import keyring
-import re
+import cgi
 import concurrent.futures
+import getpass
+import http.cookiejar
+import os
+import re
+from urllib.parse import unquote
+
+import keyring
+import mechanize
+from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
 
 def get_args():
@@ -29,15 +30,15 @@ def get_args():
     parser.add_argument("-u", "--user", help="user")
     parser.add_argument("-s", "--size", help="maximum file size in MB")
     parser.add_argument("-c", "--course", help="course name")
-    parser.add_argument("-o", "--overwrite", action='store_true', help="overwrite existing files")
+    parser.add_argument("-o", "--overwrite", action="store_true", help="overwrite existing files")
 
     return parser.parse_args()
 
 
 def setup_browser():
     br = mechanize.Browser()
-    cookiejar = cookielib.LWPCookieJar()
-    br.set_cookiejar(cookiejar)
+    cj = http.cookiejar.LWPCookieJar()
+    br.set_cookiejar(cj)
 
     br.set_handle_equiv(True)
     br.set_handle_gzip(True)
@@ -45,9 +46,9 @@ def setup_browser():
     br.set_handle_referer(True)
     br.set_handle_robots(False)
 
-    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-    br.addheaders = [('User-agent',
-                      'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36')]
+    br.set_handle_refresh(mechanize.HTTPRefreshProcessor(), max_time=1)
+    br.addheaders = [("User-agent",
+                      "Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0")]
 
     return br
 
@@ -85,13 +86,13 @@ def login(browser, url, user, password):
     print("Login in...")
 
     browser.select_form(nr=0)
-    browser.form['username'] = user
-    browser.form['password'] = password
+    browser.form["username"] = user
+    browser.form["password"] = password
     browser.submit(id="loginbtn")
 
     # Check if success
     url = browser.open(url)
-    success = url.get('Expires', None)
+    success = url.get("Expires", None)
     if not success:
         print("Login failed. Check your user and password and try again")
         exit(1)
@@ -115,7 +116,7 @@ def prompt_password_save(save, user, password):
 
 
 def scrape_courses(url, courses):
-    soup = bs(url, "html.parser")
+    soup = BeautifulSoup(url, "html.parser")
     for link in soup.findAll("a"):
         href = link.get("href")
         if href is not None and "/course/view.php" in href:
@@ -123,7 +124,7 @@ def scrape_courses(url, courses):
 
 
 def download_file(file, stream):
-    fh = open(file, 'wb')
+    fh = open(file, "wb")
     fh.write(stream.read())
     fh.close()
 
@@ -132,7 +133,7 @@ def download(args, download_response, file_path, file_name):
     file = os.path.join(file_path, file_name)
 
     if args.overwrite is False:
-        size_header = int(download_response.get('Content-Length', None))
+        size_header = int(download_response.get("Content-Length", None))
 
         if os.path.exists(file):
             if os.path.getsize(file) == size_header:
@@ -158,7 +159,7 @@ def process_download(link, args, path, browser, course_title, not_downloaded):
 
             # Download file and get filename from response header
             response = browser.open(link)
-            cdheader = response.get('Content-Disposition', None)
+            cdheader = response.get("Content-Disposition", None)
             if cdheader is not None:
                 value, params = cgi.parse_header(cdheader)
 
@@ -172,27 +173,28 @@ def process_download(link, args, path, browser, course_title, not_downloaded):
                 linked_files = set()
 
                 url = browser.open(link)
-                soup = bs(url, "html.parser")
+                soup = BeautifulSoup(url, "html.parser")
                 title = soup.find("h2").text
 
                 # Check for linked resources or submission files
 
-                links = soup.findAll("a", attrs={'href': re.compile("/mod_resource/content|/submission_files")})
+                links = soup.findAll("a", attrs={"href": re.compile("/mod_resource/content|/submission_files")})
 
                 for link in links:
                     href = link.get("href")
                     linked_files.add((href, title))
 
-                for resource in linked_files:
-                    response = browser.open(resource[0])
-                    filename = resource[1] + " - " + unquote(
-                        os.path.basename(resource[0]).split('?', maxsplit=1)[0])
+                if len(linked_files) > 0:
+                    for resource in linked_files:
+                        response = browser.open(resource[0])
+                        filename = resource[1] + " - " + unquote(
+                            os.path.basename(resource[0]).split("?", maxsplit=1)[0])
 
-                    if exceed_size(args, response):
-                        not_downloaded.append((resource[0], course_title))
-                        continue
+                        if exceed_size(args, response):
+                            not_downloaded.append((resource[0], course_title))
+                            continue
 
-                    download(args, response, path, sanitize_filename(filename))
+                        download(args, response, path, sanitize_filename(filename))
 
 
 def exceed_size(args, size_response):
@@ -200,7 +202,7 @@ def exceed_size(args, size_response):
         size = float(args.size)
 
         # Get file size and convert to MB
-        size_header = int(size_response.get('Content-Length', None))
+        size_header = int(size_response.get("Content-Length", None))
         file_size = size_header / (1024 * 1024)
 
         if file_size > size:
@@ -272,7 +274,7 @@ def main():
     # Check every course page
     for course in courses:
         url = br.open(course)
-        soup = bs(url, "html.parser")
+        soup = BeautifulSoup(url, "html.parser")
         course_title = soup.find("title").text
 
         # Don't check unwanted courses
@@ -282,7 +284,7 @@ def main():
             print("\nChecking for files in " + course_title)
             path = get_path(args, course_title)
 
-            links = soup.findAll("a", attrs={'href': re.compile("/mod/resource|/mod/assign")})
+            links = soup.findAll("a", attrs={"href": re.compile("/mod/resource|/mod/assign")})
 
             with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
                 for link in links:
